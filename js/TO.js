2f5c4b7e840b32fd7e64ae91a11a81e670dd23d2
Oleg-Kostjushenko <oleg.kostjushenko@gmail.com>
2022-10-02 20:55:08 +0300

    f

--- js/TO.js
+++ js/TO.js
@@ -10,3 +10,33 @@ for (let anchor4 of anchors4) {
         })
     })
 }
+
+let vipadMenu = false
+$('.list1').click(function () {
+    if (!vipadMenu) {
+        $(".li1").slideDown()
+    } else {
+        $(".li1").slideUp()
+    }
+    vipadMenu = !vipadMenu
+})
+
+let vipadMenu1 = false
+$('.list2').click(function () {
+    if (!vipadMenu1) {
+        $(".li2").slideDown()
+    } else {
+        $(".li2").slideUp()
+    }
+    vipadMenu1 = !vipadMenu1
+})
+
+let vipadMenu2 = false
+$('.list3').click(function () {
+    if (!vipadMenu2) {
+        $(".li3").slideDown()
+    } else {
+        $(".li3").slideUp()
+    }
+    vipadMenu2 = !vipadMenu2
+})
